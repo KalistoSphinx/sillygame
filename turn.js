d5126aed3cdcf59255455adc564c5d68b58df98a
KalistoSphinx <shouryadeshmukh6265@gmail.com>
2025-06-02 21:10:14 +0530

    completed prototype

---
+++ turn.js
@@ -0,0 +1,138 @@
+let killedTurn = 0
+let stop = false;
+
+let balance;
+
+let bullet = Math.floor(Math.random() * 6) + 1;
+console.log("Bullet is in chamber:", bullet);
+
+const bet = document.getElementById("bet");
+let elem = document.getElementsByTagName("video");
+let balanceText = document.getElementById("balance")
+
+if (localStorage.getItem("balance") !== null) {
+    balance = parseInt(localStorage.getItem("balance"));
+} else {
+    balance = parseInt(balanceText.innerText);
+    localStorage.setItem("balance", balance);
+}
+
+balanceText.innerHTML = balance
+
+window.onload = () => {
+    elem[0].play()
+}
+
+function waitForVideoToEnd(video) {
+    return new Promise(resolve => {
+        video.onended = () => {
+            resolve();
+        };
+    });
+}
+
+let turn = Math.floor(Math.random() * 2)
+
+bet.addEventListener("click", () => {
+
+    const userChoice = document.getElementById("turn").value
+    console.log(userChoice);
+    
+    
+    let amount = document.getElementById("amount").value;
+    amount = parseInt(amount);
+    
+    if(isNaN(amount)){
+        alert("Please enter an amount")
+        return
+    }
+
+    if(amount > balance){
+        alert("insuffecient balance")
+        return
+    } else {
+        balance -= amount
+        balanceText.innerHTML = balance
+    }
+
+    function delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function playSequence(bullet, elem) {
+
+        for (let i = 1; i <= 6; i++) {
+            console.log("Round:", i);
+
+            if (turn == 0) {
+                if (i === bullet) {
+                    elem[0].src = "videos/Kill1.mp4";
+                    setTimeout(() => {
+                        elem[0].play();
+                    }, 2000);
+                    await waitForVideoToEnd(elem[0]);
+                    killedTurn = i;
+                    break;
+                } else {
+                    elem[0].src = "videos/Dry1.mp4";
+                    setTimeout(() => {
+                        elem[0].play();
+                    }, 2000);
+                    await waitForVideoToEnd(elem[0]);
+                }
+            } 
+            
+            if(turn == 1){
+                if (i === bullet) {
+                    elem[0].src = "videos/Kill2.mp4";
+                    setTimeout(() => {
+                        elem[0].play();
+                    }, 2000);
+                    await waitForVideoToEnd(elem[0]);
+                    killedTurn = i;
+                    break;
+                    
+                } else {
+                    elem[0].src = "videos/Dry2.mp4";
+                    setTimeout(() => {
+                        elem[0].play();
+                    }, 2000);
+                    await waitForVideoToEnd(elem[0]);
+                }
+            }
+
+            if(turn == 0){
+                turn = 1
+            } else {
+                turn = 0
+            }
+
+            await delay(5000);
+        }
+
+        let result = document.getElementById("result");
+        bet.innerHTML = "Try again"
+
+        if (userChoice == killedTurn) {
+            console.log("You won");
+            result.innerHTML = `You Won<br>${amount} x 5: ${amount*5}rs 🤑`;
+            result.style.color = "green";
+
+            let newAmount = amount * 5
+            balance += newAmount
+            balanceText.innerHTML = balance
+        } else {
+            console.log("You lost");
+            result.innerHTML = "You Lost! 🤣";
+            result.style.color = "red";
+        }
+
+        localStorage.setItem("balance", balance)
+
+        bet.addEventListener("click", () => {
+            location.reload()
+        })
+    }
+
+    playSequence(bullet, elem);
+});
